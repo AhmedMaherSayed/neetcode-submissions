d338e748a031ab6d310f91622e6b6ad7bebea6e0
Ahmed Maher <62140280+AhmedMaherSayed@users.noreply.github.com>
2026-06-24 01:04:44 +0300

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        if (strs is null || strs.Length == 0)
+            return string.Empty;
+        if (strs.Length == 1)
+            return strs[0];
+
+        for (int i = 0; i < strs[0].Length; i++) {
+            for (int j = 1; j < strs.Length; j++) {
+                if (i >= strs[j].Length || strs[0][i] != strs[j][i])
+                    return strs[0].Substring(0, i);
+            }
+        }
+
+        return strs[0];
+    }
+}
